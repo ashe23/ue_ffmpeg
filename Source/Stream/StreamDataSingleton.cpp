6b52e7ab9dd69ca73529382be76e7cae80258611
Mher <mher.didaryan@betconstruct.com>
2018-04-11 16:12:36 +0400

    merge with dev

---
+++ Source/Stream/StreamDataSingleton.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StreamDataSingleton.h"
+
+
+UStreamDataSingleton::UStreamDataSingleton(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	CanStream = false;
+}
+
